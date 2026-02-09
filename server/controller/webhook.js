65b90063fdff0ac8dc09e6de5484bac3e747fac2
Suvojit manna <mannasuvojit84@gmail.com>
2026-02-09 08:17:41 +0530

    STRIPE_WEBHOOK_SECRET modify

--- server/controller/webhook.js
+++ server/controller/webhook.js
@@ -3,7 +3,7 @@ import Transition from "../models/transition.js";
 import User from "../models/user.js";
 
 export const stripeWebhooks = async (req, res) => {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+  const stripe = new Stripe(process.env.STRIPE_WEBHOOK_SECRET);
   const sig = req.headers["stripe-signature"];
 
   let event;
